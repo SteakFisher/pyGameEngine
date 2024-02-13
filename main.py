fae59fdc330da286b0ad320e1cdd9df34f157147
SteakFisher <miniminter2018p@gmail.com>
2024-02-14 02:41:59 +0530

    Filled in surfaces

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ from utils.types import Position, Rotation, Scaling
 
 pygame.init()
 
-screen_width = 1280
-screen_height = 720
+screen_width = 1920
+screen_height = 1080
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 running = True
 
 environment: List[Object] = []
-cube1: Cube = Cube(environment, screen, Position(1.5, 0, 7), Rotation(0, 0, 0))
+cube1: Cube = Cube(environment, screen, Position(0, 0, 7), Rotation(0, 0, 0))
 
 while running:
     for event in pygame.event.get():
@@ -26,8 +26,8 @@ while running:
 
     screen.fill("black")
 
-    # cube1.setPosition(cube1.pos + Position(0, 0, 0.01))
-    cube1.setRotation(cube1.rot + Rotation(1, 1, 0))
+    cube1.setPosition(cube1.pos + Position(0.01, 0, 0.01))
+    cube1.setRotation(cube1.rot + Rotation(1, 1, 1))
     # cube1.setScale(cube1.scale + Scaling(0.01, 0.01, 0.01))
 
     pygame.draw.circle(screen, Color(255, 255, 255), (screen.get_width() / 2, screen.get_height() / 2), 5)
--- newClasses/Cube.py
+++ newClasses/Cube.py
@@ -32,6 +32,14 @@ class Cube(Object):
 
             [6, 7]
         ]
+        self.faces = [
+            [0, 3, 1, 5],
+            [0, 8, 2, 4],
+            [1, 9, 2, 6],
+            [3, 10, 4, 7],
+            [5, 11, 6, 7],
+            [8, 11, 9, 10]
+        ]
 
     @staticmethod
     def __getUnitCube():
@@ -59,6 +67,24 @@ class Cube(Object):
     def __drawEdges(self, pixels):
         for i in self.edges:
             pygame.draw.line(self.screen, Color(255, 255, 255), pixels[i[0]], pixels[i[1]], 2)
+
+    def __drawFaces(self, pixels):
+        # pygame.draw.polygon(self.screen, Color(0,255,0), ((15, 65), (86, 125), (250, 375), (400, 25), (60, 540)))
+
+        Colors = [Color(0, 0, 0), Color(0, 0, 255), Color(0, 255, 0), Color(0, 255, 255), Color(255, 0, 0),
+                  Color(255, 0, 255), Color(255, 255, 0), Color(255, 255, 255)]
+
+        i = 0
+
+        for face in self.faces:
+            surface = []
+            for edge in face:
+                surface.append(pixels[self.edges[edge][0]])
+                surface.append(pixels[self.edges[edge][1]])
+
+            pygame.draw.polygon(self.screen, Colors[i], tuple(surface))
+            i += 1
+
 
     def draw(self):
         screen = self.screen
@@ -93,8 +119,6 @@ class Cube(Object):
         for vertex in transformedVertices:
             vertex = numpy.dot(projection_matrix, vertex)
 
-            print(vertex / vertex[3])
-
             vertex /= vertex[3]
 
             screen_x = vertex[0] * screen.get_width() / 2 + screen.get_width() / 2
@@ -106,8 +130,7 @@ class Cube(Object):
             pygame.draw.circle(screen, Color(255, 255, 255), pixel, 5)
 
         self.__drawEdges(pixels)
-
-
+        self.__drawFaces(pixels)
 
     def tick(self):
         self.draw()
